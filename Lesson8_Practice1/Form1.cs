fe865b74f9c60452fbac86de35c5affffd79a555
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-08 20:57:13 +0200

    upd

--- Lesson8_Practice1/Form1.cs
+++ Lesson8_Practice1/Form1.cs
@@ -18,7 +18,23 @@ namespace Lesson8_Practice1
             g.DrawRectangle(new Pen(Brushes.Blue, 5), 50, 50, 50, 50);
             g.DrawEllipse(new Pen(Brushes.Green, 5), 120, 50, 50, 50);
             g.DrawPolygon(new Pen(Brushes.Red, 5), points);
+
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            g.FillRectangle(blueBrush, 50, 120, 50, 50);
+            SolidBrush greenBrush = new SolidBrush(Color.Green);
+            g.FillEllipse(greenBrush, 120, 120, 50, 50);
+
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            Point[] trianglePoints = new Point[]
+            {
+                 new Point(180, 170),
+                 new Point(250, 170),
+                 new Point(200, 110)
+            };
+            g.FillPolygon(redBrush, trianglePoints);
+
             g.Dispose();
+
         }
     }
 }
